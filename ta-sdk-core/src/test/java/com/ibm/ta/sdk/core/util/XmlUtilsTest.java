3f2399e86faabc83f2bf8bfd703b303a16dffd40
Jianfeng Kong <jianfeng@ca.ibm.com>
2020-02-18 15:45:42 -0500

    Issue#5 - Add Junit test class for XmlUtils. Add Junit5  test cases to test the method in XmlUtils.java
    Signed-off-by: Jianfeng <jianfeng@ca.ibm.com>

--- ta-sdk-core/src/test/java/com/ibm/ta/sdk/core/util/XmlUtilsTest.java
+++ ta-sdk-core/src/test/java/com/ibm/ta/sdk/core/util/XmlUtilsTest.java
@@ -1,32 +1,78 @@
 package com.ibm.ta.sdk.core.util;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.List;
+import org.junit.jupiter.api.Test;
+import org.xml.sax.SAXException;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class XmlUtilsTest {
 
-    public static void main(String[] argu) throws Exception{
-        System.out.println("start");
-        File xmlFile = new File(XmlUtilsTest.class.getClassLoader().getResource("src/test/resources/sample/ejb.xml").getFile());
-        Document sampleDoc = XmlUtils.getXmlDoc(xmlFile);
-        System.out.println(XmlUtils.getDocumentTypeSystemId(sampleDoc));
-        List<Node> dtcNodes = XmlUtils.getDoctypeDTDReference(sampleDoc, xmlFile.getName(), new String[]{"ejb.xml"}, "http://java.sun.com/dtd/ejb-jar_2_0.dtd", false);
-        System.out.println(dtcNodes.size());
-        List<Node> tagNodes = XmlUtils.getTagDeclarations(sampleDoc, "", new String[]{"ejb-jar"});
-        System.out.println(tagNodes);
-        List<Node> findNodes = XmlUtils.getTagDeclarationsByAttributeValue(tagNodes, "", "version", "2.1");
-        System.out.println(findNodes);
-        findNodes = XmlUtils.getTagDeclarationsByAttributeValue(sampleDoc, xmlFile.getName(), new String[]{"(.*/)?ejb\\.xml"}, "", new String[]{"ejb-jar"}, "", "version", "2.1");
-        System.out.println(findNodes);
-        List<Node> ejbNodes = XmlUtils.getTagDeclarations(sampleDoc, "", new String[]{"enterprise-beans"});
-        String ejbName = XmlUtils.getNodeValue(XmlUtils.getFirstChildNode(ejbNodes.get(0), "message-driven"), "ejb-class");
-        //System.out.println(ejbName);
-        List<Node> ejbClassNodes = XmlUtils.getTagDeclarations(sampleDoc, "", new String[]{"ejb-class"});
-        String ejb_class = ejbClassNodes.get(0).getTextContent();
-        System.out.println(ejb_class);
+    private static File xmlFile = new File(XmlUtilsTest.class.getClassLoader().getResource("sample/ejb.xml").getFile());
+    private Document sampleDoc = null;
 
+    @BeforeEach
+    void initialXmlDoc() throws IOException, SAXException, ParserConfigurationException  {
+        sampleDoc = XmlUtils.getXmlDoc(xmlFile);
+    }
+
+    @Test
+    public void getXmlDocTest() {
+        assertNotNull(sampleDoc, "xml document get from a file is null");
+        assertEquals(sampleDoc.getXmlVersion(), "1.0");
+        assertEquals(sampleDoc.getXmlEncoding(), "UTF-8");
+    }
+
+    @Test
+    public void getDoctypeDTDReferenceTest() {
+        List<Node> dtcNodes = XmlUtils.getDoctypeDTDReference(sampleDoc, xmlFile.getName(), new String[]{"ejb.xml"}, "http://java.sun.com/dtd/ejb-jar_2_0.dtd", false);
+        assertFalse(dtcNodes.isEmpty());
+        assertEquals(dtcNodes.size(), 1);
+    }
+
+    @Test
+    public void getTagDeclarationsTest() {
+        List<Node> tagNodes = XmlUtils.getTagDeclarations(sampleDoc, "", new String[]{"ejb-jar"});
+        assertFalse(tagNodes.isEmpty());
+        assertEquals(tagNodes.size(), 1);
+    }
+
+    @Test
+    public void getTagTextTest(){
+        List<Node> ejbClassNodes = XmlUtils.getTagDeclarations(sampleDoc, "", new String[]{"ejb-class"});
+        assertFalse(ejbClassNodes.isEmpty());
+        assertEquals(ejbClassNodes.size(), 1);
+        String ejb_class = ejbClassNodes.get(0).getTextContent();
+        assertEquals(ejb_class, "samples.mdb.ejb.MessageBean");
+    }
+
+    @Test
+    public void getTagDeclarationsByAttributeValueTest() {
+        List<Node> tagNodes = XmlUtils.getTagDeclarations(sampleDoc, "", new String[]{"ejb-jar"});
+        List<Node> findNodes = XmlUtils.getTagDeclarationsByAttributeValue(tagNodes, "", "version", "2.1");
+        assertFalse(findNodes.isEmpty());
+        assertEquals(findNodes.size(), 1);
+    }
+
+    @Test
+    public void getNodeValueTest() {
+        List<Node> ejbNodes = XmlUtils.getTagDeclarations(sampleDoc, "", new String[]{"enterprise-beans"});
+        String ejbName = XmlUtils.getNodeValue(XmlUtils.getFirstChildNode(ejbNodes.get(0), "message-driven"), "ejb-name");
+        assertEquals(ejbName, "MessageBean");
+    }
+
+    @Test
+    public void getTagUsingRegExTest() {
+        List<Node> tagNodes = XmlUtils.getTagDeclarationsByAttributeValue(sampleDoc, xmlFile.getName(), new String[]{"(.*/)?ejb\\.xml"}, "", new String[]{"ejb-jar"}, "", "version", "2.1");
+        assertFalse(tagNodes.isEmpty());
+        assertEquals(tagNodes.size(), 1);
     }
 }
